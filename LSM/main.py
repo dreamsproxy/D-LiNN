2bdd4f0d1c5a419c984670fc70cfcc8388b9731f
dreamsproxy <changfengjui@gmail.com>
2023-11-08 23:32:15 +0800

    Overhaul

--- LSM/main.py
+++ LSM/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 from tqdm import tqdm
-import ffmpeg
+import random
+import os
+import multiprocessing
+
 class WeightMatrix:
-    def __init__(self, n_neurons, w_init = "zeros"):
+    def __init__(self, n_neurons: int, w_init: str = "default"):
         self.n_neurons = n_neurons
-        if w_init == "zeros" or w_init == None:
-            self.matrix = np.zeros(shape=(n_neurons, n_neurons))
-        elif w_init == "mid" or w_init == None:
+        if w_init == "default":
             self.matrix = np.zeros(shape=(n_neurons, n_neurons))+np.float16(0.5)
         elif w_init == "random":
             self.matrix = np.random.rand(n_neurons, n_neurons)
@@ -17,30 +18,34 @@ class WeightMatrix:
         pairs = [[p, p] for p in range(n_neurons)]
         for p1, p2 in pairs:
             self.matrix[p1, p2] = 0
-        
-        # Each neuron will be assigned an X (row) number for referencing the matrix
-
-        # Find a way to update the weights and how the neuron signals propagate to each other
 
     def PrintMatrix(self):
         print(self.matrix)
-    
+
 class LIF:
-    def __init__(self, neuron_id: str, trim_lim: int = 10) -> None:
+    def __init__(self, neuron_id: str, lif_init: str = "default", trim_lim: int = 10, verbose_log: bool = False) -> None:
         self.neuron_id = neuron_id
+        
         # Define simulation parameters
-        self.dT = 0.1  # Time step
-        self.tau_m = np.float16(10.0)  # Membrane time constant
-        self.V_reset = np.float16(0.0)  # Reset voltage
-        self.V_threshold = np.float16(1.0)  # Spike threshold
+        if lif_init == "default":
+            self.tau_m = np.float16(10.0)  # Membrane time constant
+            self.V_reset = np.float16(0.0)  # Reset voltage
+            self.V_threshold = np.float16(1.0)  # Spike threshold
+        elif lif_init == "random":
+            self.tau_m = np.float16(random.uniform(5, 15))  # Membrane time constant
+            self.V_reset = np.float16(random.uniform(-0.314, 0.314))  # Reset voltage
+            self.V_threshold = np.float16(random.uniform(1.0, 3.14))  # Spike threshold
 
         self.V = list()
         self.spike_log = list()
         self.spike_bool = False
         self.trim_lim = trim_lim
+        
+        self.verbose_log = verbose_log
+        self.full_spike_log = list()
 
     # Define a function to update the LIF neuron's state
-    def update(self, current_input = np.float16(0)):
+    def update(self, current_input: np.float16 = np.float16(0)):
         if len(self.spike_log) >= self.trim_lim:
             del self.spike_log[0]
         else:
@@ -55,206 +60,133 @@ class LIF:
 
         if self.V[-1] >= self.V_threshold:
             self.V[-1] = self.V_reset
-            self.spike_log.append("1")
+            self.spike_log.append(self.V_threshold)
             self.spike_bool = True
+            if self.verbose_log:
+                self.full_spike_log.append(1)
         else:
-            self.spike_log.append("0")
+            self.spike_log.append(self.V_threshold)
             self.spike_bool = False
-
+            if self.verbose_log:
+                self.full_spike_log.append(0)
 
 class Network:
-    def __init__(self, n_neurons, w_init = None, hist_lim = 10) -> None:
+    def __init__(self, n_neurons: int, lif_init: str = "default", w_init: str = "default", hist_lim: int = 10) -> None:
+        self.stdp_lr = 0.01
+        self.hebbian_lr = 0.00004
+
         self.n_neurons = n_neurons
         self.LIFNeurons = dict()
         self.weightsclass = WeightMatrix(n_neurons, w_init)
-        self.weightmatrix = self.weightsclass.matrix
-        self.wave_dict = dict()
+        self.weight_matrix = self.weightsclass.matrix
         self.weight_log = []
-        self.spike_mem = {}
+
         self.hist_lim = hist_lim
+        self.lif_init = lif_init
 
     def InitNetwork(self):
         for i in range(self.n_neurons):
-            self.LIFNeurons[i] = LIF(i, trim_lim=self.hist_lim)
-
-    def PrepPropagation(self, neuron_id):
-        neuron_keys = list(self.LIFNeurons.keys())
-        neighbor_weights = self.weightmatrix[neuron_id, :]
-        del neuron_keys[neuron_id]
-
-        return neuron_keys, neighbor_weights
+            self.LIFNeurons[i] = LIF(i, trim_lim=self.hist_lim, lif_init = self.lif_init)
 
     def Decay(self, n1: str, n2: str, factor: float):
-        # TODO
-        # Implement a method where decay factor is relative to
-        # the tick (T)
         factor = np.float16(factor)
-        old_weight = self.weightmatrix[n1, n2]
-        self.weightmatrix[n1, n2] -= factor * old_weight
+        old_weight = self.weight_matrix[n1, n2]
+        self.weight_matrix[n1, n2] -= factor * old_weight
 
     def Hebbian(self, n1: str, n2: str):
         # Neurons that fire together,
         # Connects together.
-        source_act = self.LIFNeurons[n1].V_threshold
-        target_act = self.LIFNeurons[n2].V_threshold
 
-        old_weight = self.weightmatrix[n1, n2]
-        if old_weight < np.float16(1.000):
-            new_weight = old_weight + (old_weight * np.float16(0.1))
-            if new_weight >= np.float16(1.000):
-                new_weight = np.float16(1.000)
-                self.weightmatrix[n1, n2] = new_weight
-            elif new_weight < np.float16(1.0):
-                self.weightmatrix[n1, n2] = new_weight
-        elif old_weight >= np.float16(1.000):
-            new_weight = np.float16(1.000)
-            self.weightmatrix[n1, n2] = new_weight
+        latest_pre_synaptic_spikes = self.LIFNeurons[n1].spike_log
+        latest_post_synaptic_spikes = self.LIFNeurons[n2].spike_log
 
-    def step(self, tick,  input_current = np.float16(0.000), input_neuron = 0):
+        correlation_term = np.dot(latest_pre_synaptic_spikes, latest_post_synaptic_spikes)
+        new_weight = self.weight_matrix[n1][n2] + np.dot(self.hebbian_lr, correlation_term)
+        if new_weight <= 0.25:
+            self.Decay(n1, n2, factor=self.hebbian_lr)
+        else:
+            self.weight_matrix[n1][n2] += new_weight
+
+    def step(self, input_current = np.float16(0.000), input_neuron:str = "0"):
         neuron_keys = list(self.LIFNeurons.keys())
         for k in neuron_keys:
-            spike_collector = []
             neu = self.LIFNeurons[k]
-            if neu.neuron_id == input_neuron:
+            if neu.neuron_id == str(input_neuron):
                 neu.update(input_current)
             else:
                 neu.update(np.float16(0))
 
-            # Collect IDs of all neurons that spiked.
-            if neu.spike_bool:
-                spike_collector.append(neu.neuron_id)
+        # Do Global Weight Update
+        for k1 in neuron_keys:
+            for k2 in neuron_keys:
+                if k1 != k2:
+                    self.Hebbian(k1, k2)
 
-            # Do Global Weight Update
-            for n1 in spike_collector:
-                for n2 in spike_collector:
-                    if n1 != n2:
-                        self.Hebbian(n1, n2)
-            
-            # Decay is only called every 'hist_lim' ticks
-            if tick % self.hist_lim:
-                # Filter the neurons that did not fire.
-                non_fire_neurons_list = [x for x in neuron_keys if x not in spike_collector]
-                # Decay the neurons that did not fire.
-                for non_fire_id in non_fire_neurons_list:
-                    if "1" not in self.LIFNeurons[non_fire_id].spike_log:
-                        for n2 in non_fire_neurons_list:
-                            if non_fire_id != n2:
-                                self.Decay(non_fire_id, n2, factor=0.001)
-        #print(self.weightmatrix.shape)
-        self.weight_log.append(np.copy(self.weightmatrix))
+        # Normalize the weights to prevent uncontrolled growth
+        self.weight_matrix /= np.max(np.abs(self.weight_matrix))
+        # Scale the weights to keep it between 0.0 and 1.0
+        self.weight_matrix = (self.weight_matrix-np.min(self.weight_matrix))/(np.max(self.weight_matrix)-np.min(self.weight_matrix))
 
-    def SaveWeightTables(self):
-        import pandas as pd
-        cols = list(self.LIFNeurons.keys())
-        for tick, table in tqdm(enumerate(self.weight_log), total=len(self.weight_log)):
-            frame = pd.DataFrame(table)
-            frame.columns = cols
-            frame.set_index(cols)
-            frame.to_csv(f"./weight_logs/{tick} WM.csv")
+        # Copy weight matrix to a logger
+        self.weight_log.append(np.copy(self.weight_matrix))
 
-    def PlotWeightMatrix(self):
-        import numpy as np
-        import seaborn as sns
-        import matplotlib.pyplot as plt
-        from matplotlib import animation
-        num_frames = len(self.weight_log)
-        print(num_frames)
-        
-        # Set the background color for the plot
-        sns.set(rc={'axes.facecolor':'#002439', 'figure.facecolor':'#002439'})
+        # NOTE: WARNING START
+        #
+        # NOTE: When running on long periods of time!
+        # NOTE: spawn a separate process to write the logs!
+        #
+        # NOTE: WARNING END
 
-        # extract initial frame
-        init_frame = self.weight_log[0]
-        self.weight_log.pop(0)
-        def init():
-            # Initialize the heatmap (use the first frame as the initial state)
-            heatmap = sns.heatmap(
-                init_frame,
-                square=True,
-                cmap="mako",
-                annot=True,
-                annot_kws={'size': 8},
-                fmt = ".2f"
-                )
-            heatmap.invert_yaxis()
-            heatmap.set_xticklabels(heatmap.get_xticklabels(), color="white")
-            heatmap.set_yticklabels(heatmap.get_yticklabels(), color="white")
-            heatmap.set_title("Weight Matrix").set_color("white")
-            heatmap.title.set_fontsize(20)
-
-        fig = plt.figure()
-
-        def animate(i):
-            data = self.weight_log[i]
-            sns.heatmap(data, square=True, cmap="mako", annot=True, annot_kws={'size': 8}, fmt = ".2f", cbar=False)
-
-        anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames-1, repeat=True)
-        save_prog = tqdm(total = num_frames)
-        
-        anim.save("mat.gif", fps=2, progress_callback=save_prog.update(1))
-
+    def SaveWeightTables(self, mode = "npy"):
+        if mode == "csv":
+            import pandas as pd
+            cols = list(self.LIFNeurons.keys())
+            for tick, table in tqdm(enumerate(self.weight_log), total=len(self.weight_log)):
+                frame = pd.DataFrame(table)
+                frame.columns = cols
+                frame.set_index(cols)
+                frame.to_csv(f"./weight_logs/{tick} WM.csv")
+        elif mode == "npy":
+            total_ticks = len(self.weight_log)
+            self.weight_log = np.asarray(self.weight_log)
+            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
+            np.save("./weight_logs.npy", self.weight_log)
+        else:
+            print("No save format was defined, saving in .npy format!")
+            total_ticks = len(self.weight_log)
+            self.weight_log = np.asarray(self.weight_log)
+            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
+            np.save("./weight_logs.npy", self.weight_log)
     
-    def SaveWeightFrames(self):
-        import seaborn as sns
-        import matplotlib.pyplot as plt
-        sns.set(rc={'axes.facecolor':'#002439', 'figure.facecolor':'#002439'})
-        for step, frame in enumerate(self.weight_log):
-            plt.clf()
-            fig = sns.heatmap(frame, square=True, cmap="deep", annot=True, annot_kws={'size': 8})
-            fig.invert_yaxis()
-            fig.set_xticklabels(fig.get_xticklabels(), color="white")
-            fig.set_yticklabels(fig.get_yticklabels(), color="white")
-            
-            fig.set_title("Weight Matrix").set_color("white")
-            fig.title.set_fontsize(20)
-            fig.figure.savefig(f"{step}.png", dpi = 1200)
-            plt.close()  # Close the figure to release resources
-
-    def PrintNetworkV(self):
-        neuron_keys = list(self.LIFNeurons.keys())
-        for i in neuron_keys:
-            print(self.LIFNeurons[i].V)
-
-    def PlotNetworkV(self):
-        import plotly.graph_objects as go
+    def SaveNeuronPotentials(self):
+        format_cache = []
+        for k in list(self.LIFNeurons.keys):
+            format_cache.append(np.asarray(self.LIFNeurons[k].V))
+        format_cache = np.asarray(format_cache)
+        np.save("./neuron_V_logs.npy", format_cache)
+    
+    def SaveNeuronSpikes(self):
+        # Check if the neurons are logged verbosely
+        if len(self.LIFNeurons[list(self.LIFNeurons.keys)[0]]) <= 1:
+            e = Exception("Neurons were not initialized with 'verbose_log' to 'True';!")
+            raise e
+        format_cache = []
+        for k in list(self.LIFNeurons.keys):
+            format_cache.append(np.asarray(self.LIFNeurons[k].full_spike_log))
+        format_cache = np.asarray(format_cache)
+        np.save("./neuron_spike_logs.npy",format_cache)
         
-        fig = go.Figure()
-        for key, data in self.LIFNeurons.items():
-            fig.add_trace(
-                go.Scatter(
-                    x=list(range(len(data.V))),
-                    y=data.V, mode='lines',
-                    name=key))
-        fig.update_layout(title='Membrane Potential Log',
-                        xaxis_title='Ticks',
-                        yaxis_title='Voltage')
-        fig.show()
-
 
 if __name__ == "__main__":
-    snn = Network(n_neurons = 10, w_init="random", hist_lim=20)
+    snn = Network(n_neurons = 128, lif_init = "random", w_init="random", hist_lim=17)
     snn.InitNetwork()
-    #snn.step(np.float16(10.0), 0)
-    #raise
-    for i in tqdm(range(100)):
-        if i % 10 == 0:
-            snn.step(tick = i, input_current= np.float16(10.0), input_neuron= 0)
-        if i % 30 == 0:
-            snn.step(tick = i, input_current= np.float16(50.0), input_neuron= 0)
+
+    for i in tqdm(range(1000)):
+        if i == random.randint(0, 1000):
+#        if i % 10 == 0:
+            snn.step(input_current= np.float16(10.0), input_neuron= 0)
+#        elif i % 30 == 0:
+#            snn.step(input_current= np.float16(10.0), input_neuron= 0)
         else:
-            snn.step(tick = i, input_current= np.float16(0.00), input_neuron= 0)
-    #snn.PrintNetworkV()
-    #print(snn.LIFNeurons[0].V)
-    #snn.PlotNetworkV()
-    #snn.SaveWeightFrames()
+            snn.step(input_current= np.float16(0.00), input_neuron= 0)
     snn.SaveWeightTables()
-    #snn.PlotWeightMatrix()
-    #print(snn.weight_log[0])
-    #print()
-    #print(snn.weight_log[-1])
-    #print(len(snn.weight_log))
-    #for key in list(snn.LIFNeurons.keys()):
-    #    print(f"{key}\t{snn.LIFNeurons[key].spikes}")
-    #print(snn.global_spike_log)
-    #snn.GetNeuronWeights()
-    #WeightMatrix.PrintMatrix()
--- LSM/visualize.py
+++ LSM/visualize.py
@@ -6,10 +6,13 @@ from matplotlib import pyplot as plt
 
 file_list = glob("./weight_logs/*WM.csv")
 
+"""
 preproccess = []
+
 for csv_file in file_list:
     temp = pd.read_csv(csv_file).transpose()
     temp = temp.drop(temp.index[0])
+    preproccess.append(temp)
 
 weight_log = []
 for frame in preproccess:
@@ -22,23 +25,37 @@ for frame in preproccess:
 #print(weight_log.head())
 
 #sns.lineplot(weight_log)
-#plt.show()
+#plt.show()"""
+
+weight_logs = []
+weight_matrix = np.load("./weight_logs.npy")
+#print(weight_matrix.shape)
+#print(weight_matrix[0])
+for tick_slice in weight_matrix:
+    df_tick_slice = pd.DataFrame(tick_slice, columns=[str(x) for x in range(tick_slice.shape[0])])
+    weight_logs.append(df_tick_slice)
 
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-3
+
 
 # generate the frames. NB name
 frames = [
-    go.Frame(data=go.Heatmap(z=preproccess.values, x=preproccess.columns, y=preproccess.index), name=i)
-    for i, preproccess in enumerate(preproccess)
+    go.Frame(
+        data=go.Heatmap(
+            z=frame.values,
+            x=frame.columns,
+            y=frame.index,
+            colorscale="viridis"),
+        name=i)
+    for i, frame in enumerate(weight_logs)
 ]
 
 fig = go.Figure(data=frames[0].data, frames=frames).update_layout(
     updatemenus=[
         {
-            "buttons": [{"args": [None, {"frame": {"duration": 500, "redraw": True}}],
+            "buttons": [{"args": [None, {"frame": {"duration": 5, "redraw": True}}],
                          "label": "Play", "method": "animate",},
                         {"args": [[None],{"frame": {"duration": 0, "redraw": False},
                                           "mode": "immediate", "transition": {"duration": 0},},],
@@ -51,11 +68,91 @@ fig = go.Figure(data=frames[0].data, frames=frames).update_layout(
                                             "mode": "immediate",},],
                          "label": f.name, "method": "animate",}
                         for f in frames],}],
-    height=800,
-    yaxis={"title": 'callers'},
-    xaxis={"title": 'callees', "tickangle": 45, 'side': 'top'},
+    height=1000,
+    width=1000,
+    yaxis={"title": 'Pre-Synaptic IDs', "tickangle":90},
+    xaxis={"title": 'Post-Synaptic IDs', 'side': 'top'},
     title_x=0.5,
 
 )
 
 fig.show()
+raise
+
+def PlotWeightMatrix(self):
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+    num_frames = len(self.weight_log)
+    print(num_frames)
+    
+    # Set the background color for the plot
+    sns.set(rc={'axes.facecolor':'#002439', 'figure.facecolor':'#002439'})
+
+    # extract initial frame
+    init_frame = self.weight_log[0]
+    self.weight_log.pop(0)
+    def init():
+        # Initialize the heatmap (use the first frame as the initial state)
+        heatmap = sns.heatmap(
+            init_frame,
+            square=True,
+            cmap="mako",
+            annot=True,
+            annot_kws={'size': 8},
+            fmt = ".2f"
+            )
+        heatmap.invert_yaxis()
+        heatmap.set_xticklabels(heatmap.get_xticklabels(), color="white")
+        heatmap.set_yticklabels(heatmap.get_yticklabels(), color="white")
+        heatmap.set_title("Weight Matrix").set_color("white")
+        heatmap.title.set_fontsize(20)
+
+    fig = plt.figure()
+
+    def animate(i):
+        data = self.weight_log[i]
+        sns.heatmap(data, square=True, cmap="mako", annot=True, annot_kws={'size': 8}, fmt = ".2f", cbar=False)
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames-1, repeat=True)
+    save_prog = tqdm(total = num_frames)
+    
+    anim.save("mat.gif", fps=2, progress_callback=save_prog.update(1))
+
+
+def SaveWeightFrames(self):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.set(rc={'axes.facecolor':'#002439', 'figure.facecolor':'#002439'})
+    for step, frame in enumerate(self.weight_log):
+        plt.clf()
+        fig = sns.heatmap(frame, square=True, cmap="deep", annot=True, annot_kws={'size': 8})
+        fig.invert_yaxis()
+        fig.set_xticklabels(fig.get_xticklabels(), color="white")
+        fig.set_yticklabels(fig.get_yticklabels(), color="white")
+        
+        fig.set_title("Weight Matrix").set_color("white")
+        fig.title.set_fontsize(20)
+        fig.figure.savefig(f"{step}.png", dpi = 1200)
+        plt.close()  # Close the figure to release resources
+
+def PrintNetworkV(self):
+    neuron_keys = list(self.LIFNeurons.keys())
+    for i in neuron_keys:
+        print(self.LIFNeurons[i].V)
+
+def PlotNetworkV(self):
+    import plotly.graph_objects as go
+    
+    fig = go.Figure()
+    for key, data in self.LIFNeurons.items():
+        fig.add_trace(
+            go.Scatter(
+                x=list(range(len(data.V))),
+                y=data.V, mode='lines',
+                name=key))
+    fig.update_layout(title='Membrane Potential Log',
+                    xaxis_title='Ticks',
+                    yaxis_title='Voltage')
+    fig.show()
