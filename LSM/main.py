738dfa49d6d563e2a9858b91cfe134f2d1cdb033
dreamsproxy <changfengjui@gmail.com>
2023-11-16 14:08:46 +0800

    Fix

--- LSM/main.py
+++ LSM/main.py
@@ -102,6 +102,48 @@ class Network:
         self.lif_init = lif_init
         
         self.verbose_logging = verbose_logging
+        
+        self.step_debug_log = []
+    
+    def SaveWeightTables(self, mode = "npy"):
+        if mode == "csv":
+            import pandas as pd
+            cols = list(self.LIFNeurons.keys())
+            for tick, table in tqdm(enumerate(self.weight_log), total=len(self.weight_log)):
+                frame = pd.DataFrame(table)
+                frame.columns = cols
+                frame.set_index(cols)
+                frame.to_csv(f"./weight_logs/{tick} WM.csv")
+        elif mode == "npy":
+            total_ticks = len(self.weight_log)
+            self.weight_log = np.asarray(self.weight_log)
+            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
+            np.save("./weight_logs.npy", self.weight_log)
+        else:
+            print("No save format was defined, saving in .npy format!")
+            total_ticks = len(self.weight_log)
+            self.weight_log = np.asarray(self.weight_log)
+            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
+            np.save("./weight_logs.npy", self.weight_log)
+    
+    def SaveNeuronPotentials(self):
+        format_cache = []
+        for k in list(self.neuron_keys):
+            format_cache.append(np.asarray(self.LIFNeurons[k].V))
+        format_cache = np.asarray(format_cache)
+        np.save("./neuron_V_logs.npy", format_cache)
+    
+    def SaveNeuronSpikes(self):
+        # Check if the neurons are logged verbosely
+        len(self.LIFNeurons[0].full_spike_log)
+        if len(self.LIFNeurons[0].full_spike_log) <= 1:
+            e = Exception("Neurons were not initialized with 'verbose_log' to 'True' !")
+            raise e
+        format_cache = []
+        for k in list(self.neuron_keys):
+            format_cache.append(np.asarray(self.LIFNeurons[k].full_spike_log))
+        format_cache = np.asarray(format_cache)
+        np.save("./neuron_spike_logs.npy", format_cache)
 
     def InitNetwork(self):
         for i in range(self.n_neurons):
@@ -143,60 +185,77 @@ class Network:
 
         return cache_dict
 
-    def step(self, input_current = np.float16(0.000), input_neuron:str = "0"):
-        input_current = input_current / np.pi
-        #print("\n\nStep called")
+    def step(self, input_current = np.float16(0.0000), input_neuron:str = "0"):
+        self.step_debug_log.append("\n\nStep [START]")
+        if input_current != np.float16(0.0000):
+            self.step_debug_log.append("\tInput Current Detected!")
+            input_current = input_current / np.pi
+        else:
+            self.step_debug_log.append("\tNo Input\n")
+        
         fired_neuron_keys = list()
         signal_keys = list(self.signal_cache.keys())
 
         filtered_keys = [int(n_k) for n_k in self.neuron_keys]
-        #print(f"All Neuron Keys: {self.neuron_keys}")
-        #print(f"Filtered Keys: {signal_keys}")
+        self.step_debug_log.append(f"\tAll Neuron Keys: {self.neuron_keys}")
+        self.step_debug_log.append(f"\tBacklog Signal Keys: {signal_keys}\n")
         if len(signal_keys) > 0:
+            self.step_debug_log.append(f"\tBacklogs detected!")
+            self.step_debug_log.append(f"\tBacklog [START]")
             for receiver_neuron in signal_keys:
                 r_k = int(receiver_neuron)
                 recieved_signal = self.signal_cache[str(r_k)]
-                #print(f"\tProcessing Neuron: {r_k}")
                 neu = self.LIFNeurons[r_k]
                 if str(r_k) == str(input_neuron):
                     neu.update(input_current+recieved_signal)
-                    #print(f"\tBacklog Update: input neuron {r_k}")
+                    self.step_debug_log.append(f"\t\tUpdate: input neuron {r_k}")
                     #raise
                 else:
-                    #print(f"\tBacklog Update: neuron {r_k}")
+                    self.step_debug_log.append(f"\t\tUpdate: neuron {r_k}")
                     neu.update(np.float16(recieved_signal))
                     #raise
 
                 if neu.spike_bool:
                     fired_neuron_keys.append(r_k)
-                #print(f"\t\tRemove: {r_k} from {filtered_keys}")
+                self.step_debug_log.append(f"\t\t\tRemoved: {r_k} from {filtered_keys}")
                 filtered_keys.remove(r_k)
 
+        self.step_debug_log.append(f"\tBacklog [ENDED]")
+        self.step_debug_log.append(f"\n\tNormal Step [START]")
         for k in filtered_keys:
+            self.step_debug_log.append(f"")
             neu = self.LIFNeurons[k]
             if str(k) == str(input_neuron):
-                #print(f"\t\tUpdate: input neuron {k}")
+                self.step_debug_log.append(f"\t\tUpdate: input neuron {k}")
                 neu.update(input_current)
             else:
-                #print(f"\t\tUpdate: neuron {k}")
+                self.step_debug_log.append(f"\t\tUpdate: neuron {k}")
                 neu.update(np.float16(0))
             if neu.spike_bool:
                 fired_neuron_keys.append(k)
 
         if len(fired_neuron_keys) >= 1:
-            #print(fired_neuron_keys)
+            self.step_debug_log.append(f"\t\tThese Neurons Fired: {fired_neuron_keys}")
             self.signal_cache = self.PrepSignals(fired_neuron_keys)
-        
+        else:
+            self.step_debug_log.append(f"\t\tNO Neurons Fired: {fired_neuron_keys}")
+
+        self.step_debug_log.append(f"\tNormal Step [ENDED]")
+
+        self.step_debug_log.append(f"\n\tGLOBAL HEBBIAN WEIGHT OPT [START]")
         # Do Global Weight Update
         for k1 in self.neuron_keys:
             for k2 in self.neuron_keys:
                 if k1 != k2:
                     self.Hebbian(k1, k2)
+        self.step_debug_log.append(f"\tGLOBAL HEBBIAN WEIGHT OPT [ENDED]")
 
+        self.step_debug_log.append(f"\n\tWeight Normalization [START]")
         # Normalize the weights to prevent uncontrolled growth
         self.weight_matrix /= np.max(np.abs(self.weight_matrix))
         # Scale the weights to keep it between 0.0 and 1.0
         self.weight_matrix = (self.weight_matrix-np.min(self.weight_matrix))/(np.max(self.weight_matrix)-np.min(self.weight_matrix))
+        self.step_debug_log.append(f"\tWeight Normalization [ENDED]")
 
         # Copy weight matrix to a logger
         self.weight_log.append(np.copy(self.weight_matrix))
@@ -209,47 +268,87 @@ class Network:
         # NOTE: WARNING END
         del fired_neuron_keys
         del filtered_keys
+        self.step_debug_log.append(f"Step [END]")
 
-    def SaveWeightTables(self, mode = "npy"):
-        if mode == "csv":
-            import pandas as pd
-            cols = list(self.LIFNeurons.keys())
-            for tick, table in tqdm(enumerate(self.weight_log), total=len(self.weight_log)):
-                frame = pd.DataFrame(table)
-                frame.columns = cols
-                frame.set_index(cols)
-                frame.to_csv(f"./weight_logs/{tick} WM.csv")
-        elif mode == "npy":
-            total_ticks = len(self.weight_log)
-            self.weight_log = np.asarray(self.weight_log)
-            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
-            np.save("./weight_logs.npy", self.weight_log)
-        else:
-            print("No save format was defined, saving in .npy format!")
-            total_ticks = len(self.weight_log)
-            self.weight_log = np.asarray(self.weight_log)
-            np.reshape(self.weight_log, (total_ticks, self.n_neurons, self.n_neurons))
-            np.save("./weight_logs.npy", self.weight_log)
-    
-    def SaveNeuronPotentials(self):
-        format_cache = []
-        for k in list(self.neuron_keys):
-            format_cache.append(np.asarray(self.LIFNeurons[k].V))
-        format_cache = np.asarray(format_cache)
-        np.save("./neuron_V_logs.npy", format_cache)
-    
-    def SaveNeuronSpikes(self):
-        # Check if the neurons are logged verbosely
-        len(self.LIFNeurons[0].full_spike_log)
-        if len(self.LIFNeurons[0].full_spike_log) <= 1:
-            e = Exception("Neurons were not initialized with 'verbose_log' to 'True' !")
-            raise e
-        format_cache = []
-        for k in list(self.neuron_keys):
-            format_cache.append(np.asarray(self.LIFNeurons[k].full_spike_log))
-        format_cache = np.asarray(format_cache)
-        np.save("./neuron_spike_logs.npy", format_cache)
+    def Audio(self):
+        import pyaudio
+        import wave
+        import struct
+        import matplotlib.pyplot as plt
         
+        def isSilence(windowPosition):
+            sumVal = sum( [ x*x for x in sound[windowPosition:windowPosition+windowSize+1] ] )
+            avg = sumVal/(windowSize)
+            if avg <= 0.0001:
+                return True
+            else:
+                return False
+        self.CHUNK = 1024*2
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.RECORD_SECONDS = 5
+        WAVE_OUTPUT_FILENAME = "output.wav"
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+
+        frames = []
+
+        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+            data = stream.read(self.CHUNK)
+            data = struct.unpack(str(2 * self.CHUNK) + 'B', data)
+            # create np array and offset by 128
+            frames.append(data)
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        
+        frames = [np.array(data, dtype='b')[::2] for data in frames]
+        frames = [data + 127 for data in frames]
+        #detect silence and notes
+        i=0
+        windowSize = 2205
+        windowPosition = 0
+        listOfLists = []
+        listOfLists.append([])
+        maxVal = len(frames) - windowSize
+        while True:
+            if windowPosition >= maxVal:
+                break
+            if not isSilence(windowPosition):
+                while not isSilence(windowPosition):
+                    listOfLists[i].append(frames[windowPosition:windowPosition+ windowSize+1])
+                    windowPosition += windowSize
+                listOfLists.append([]) #empty list
+                i += 1
+            windowPosition += windowSize
+        
+        frequencies = []
+        #Calculating the frequency of each detected note by using DFT
+        for signal in listOfLists:
+            if not signal:
+                break
+            w = np.fft.fft(signal)
+            freqs = np.fft.fftfreq(len(w))
+            l = len(signal)
+
+            #imax = index of first peak in w
+            imax = np.argmax(np.abs(w))
+            fs = freqs[imax]
+
+            freq = imax*fs/l
+            frequencies.append(freq)
+        
+        plt.plot(frames)
+        plt.show()
+        return frames
+
 
 if __name__ == "__main__":
     snn = Network(
@@ -259,16 +358,16 @@ if __name__ == "__main__":
         hist_lim=17,
         verbose_logging = True)
     snn.InitNetwork()
-    #print(snn.neuron_keys)
-    for i in tqdm(range(50)):
-        if i % 10 == 0:
+    audioframes = snn.Audio()
+    #print(audioframes)
+    print(len(audioframes))
+    raise
+    #for i in tqdm(range(50)):
+    while True:
             snn.step(input_current= np.float16(-55), input_neuron= "0")
-        #elif i % 30 == 0:
-        #    snn.step(input_current= np.float16(30.0), input_neuron= "0")
-        else:
-            snn.step(input_current= np.float16(0.00), input_neuron= "0")
+            break
     snn.SaveWeightTables()
     snn.SaveNeuronSpikes()
     snn.SaveNeuronPotentials()
-    for debug_msg in snn.LIFNeurons[1].full_debug:
+    for debug_msg in snn.step_debug_log:
         print(debug_msg)
--- LSM/visualize.py
+++ LSM/visualize.py
@@ -6,70 +6,48 @@ from matplotlib import pyplot as plt
 import plotly.graph_objects as go
 
 def WeightMatrix(path = "./weight_logs.npy"):
-    if path[-3:] == "csv":
-        # I FORGOT WHAT THIS IS
-        file_list = glob("./weight_logs/*WM.csv")
-        preproccess = []
-        for csv_file in file_list:
-            temp = pd.read_csv(csv_file).transpose()
-            temp = temp.drop(temp.index[0])
-            preproccess.append(temp)
-        weight_log = []
-        for frame in preproccess:
-            weight_log.append(frame["0"])
+    weight_logs = []
+    weight_matrix = np.load(path)
+    for tick_slice in weight_matrix:
+        df_tick_slice = pd.DataFrame(tick_slice, columns=[str(x) for x in range(tick_slice.shape[0])])
+        weight_logs.append(df_tick_slice)
+    frames = [
+        go.Frame(
+            data=go.Heatmap(
+                z=frame.values,
+                x=frame.columns,
+                y=frame.index,
+                colorscale="viridis"),
+            name=i)
+        for i, frame in enumerate(weight_logs)
+    ]
 
-        weight_log = pd.concat(weight_log, axis = 1, ignore_index= True)
-        weight_log = weight_log.transpose()
+    fig = go.Figure(data=frames[0].data, frames=frames).update_layout(
+        updatemenus=[
+            {
+                "buttons": [{"args": [None, {"frame": {"duration": 5, "redraw": True}}],
+                            "label": "Play", "method": "animate",},
+                            {"args": [[None],{"frame": {"duration": 0, "redraw": False},
+                                            "mode": "immediate", "transition": {"duration": 0},},],
+                            "label": "Pause", "method": "animate",},],
+                "type": "buttons",
+            }
+        ],
+        # iterate over frames to generate steps... NB frame name...
+        sliders=[{"steps": [{"args": [[f.name],{"frame": {"duration": 0, "redraw": True},
+                                                "mode": "immediate",},],
+                            "label": f.name, "method": "animate",}
+                            for f in frames],}],
+        height=1000,
+        width=1000,
+        yaxis={"title": "Pre-Synaptic IDs", "tickangle":90},
+        xaxis={"title": "Post-Synaptic IDs", "side": "top"},
+        title_x=0.5,
 
-        weight_log = weight_log.drop(weight_log.index[0])
-        print(weight_log.head())
+    )
 
-        sns.lineplot(weight_log)
-        plt.show()
-
-    else:
-        weight_logs = []
-        weight_matrix = np.load("./weight_logs.npy")
-        for tick_slice in weight_matrix:
-            df_tick_slice = pd.DataFrame(tick_slice, columns=[str(x) for x in range(tick_slice.shape[0])])
-            weight_logs.append(df_tick_slice)
-        frames = [
-            go.Frame(
-                data=go.Heatmap(
-                    z=frame.values,
-                    x=frame.columns,
-                    y=frame.index,
-                    colorscale="viridis"),
-                name=i)
-            for i, frame in enumerate(weight_logs)
-        ]
-
-        fig = go.Figure(data=frames[0].data, frames=frames).update_layout(
-            updatemenus=[
-                {
-                    "buttons": [{"args": [None, {"frame": {"duration": 5, "redraw": True}}],
-                                "label": "Play", "method": "animate",},
-                                {"args": [[None],{"frame": {"duration": 0, "redraw": False},
-                                                "mode": "immediate", "transition": {"duration": 0},},],
-                                "label": "Pause", "method": "animate",},],
-                    "type": "buttons",
-                }
-            ],
-            # iterate over frames to generate steps... NB frame name...
-            sliders=[{"steps": [{"args": [[f.name],{"frame": {"duration": 0, "redraw": True},
-                                                    "mode": "immediate",},],
-                                "label": f.name, "method": "animate",}
-                                for f in frames],}],
-            height=1000,
-            width=1000,
-            yaxis={"title": 'Pre-Synaptic IDs', "tickangle":90},
-            xaxis={"title": 'Post-Synaptic IDs', 'side': 'top'},
-            title_x=0.5,
-
-        )
-
-        fig.show()
-        raise
+    fig.show()
+    raise
 
 def NeuronSpikes(file_type = ".npy"):
     import plotly.express as px
@@ -85,17 +63,115 @@ def NeuronSpikes(file_type = ".npy"):
     fig.show()
 
 def NeuronPotentials():
-    import plotly.express as px
+    import plotly as plty
     v_logs = np.load("./neuron_V_logs.npy")
-    print(v_logs.shape)
+    v_logs = (v_logs-np.min(v_logs))/(np.max(v_logs)-np.min(v_logs))
+    #v_logs = np.swapaxes(v_logs, 0, 1)
     # Convert to dataframe
-    v_logs_df = pd.DataFrame(np.swapaxes(v_logs, 0, 1), columns=[i for i in range(v_logs.shape[0])])
-    v_logs_df.set_index([i for i in range(v_logs.shape[0])])
-    print(v_logs_df.head())
+    v_df = pd.DataFrame(v_logs, columns=[i for i in range(v_logs.shape[1])])
+    v_df.set_index([i for i in range(v_logs.shape[0])])
 
-    fig = px.line(v_logs_df, title="Neuron Spike Chart")
+    
+    fig = plty.tools.make_subplots(rows=1, cols=2)
+    hm1 = go.Figure(
+        data = go.Heatmap(
+            x = v_df.columns,
+            z = v_df.values,
+            y = v_df.index,
+            colorscale="viridis"
+        )
+    ).update_layout(
+        xaxis = {"title": "Tick","side": "top"},
+        yaxis = {"title": "Neuron ID"}
+    )
+    fig.add_trace(hm1.select_traces())
+
     fig.show()
+    raise
+    raise
 
+
+def Network(path = "./weight_logs.npy"):
+    import igraph as ig
+    def GenerateCoords(cx, cy, cz, radius, n_nodes=360):
+        phi = np.linspace(0, 2 * np.pi, n_nodes)
+        theta = np.linspace(0, np.pi, n_nodes)
+
+        #theta, phi = np.meshgrid(theta, phi)
+
+        x = radius * np.cos(theta) * np.sin(phi)
+        y = radius * np.sin(theta) * np.sin(phi)
+        z = radius * np.cos(phi)
+        return x, y, z
+        
+    weight_matrix = np.load(path)
+    
+    neuron_ids = [i for i in range(weight_matrix.shape[1])]
+    link_map = {}
+    Edges = []
+    
+    for n1 in neuron_ids:
+        link_pairs = []
+        for n2 in neuron_ids:
+            if n1 != n2:
+                link_pairs.append(n2)
+        link_map[n1] = link_pairs
+    
+    for i in neuron_ids:
+        pairs = link_map[i]
+        for target in pairs:
+            Edges.append((i, target))
+
+    G = ig.Graph(Edges, directed=False)
+    layt=G.layout('sphere', dim=3)
+
+    x, y, z = GenerateCoords(0.5, 0.5, 0.5, radius=0.3, n_nodes=len(neuron_ids))
+    
+    for i in range(len(neuron_ids)):
+        layt[i][0] = x[i]
+        layt[i][1] = y[i]
+        layt[i][2] = z[i]
+
+    link_coords = []
+
+    for i in range(len(neuron_ids)):
+        link_coords.append((x[i], y[i], z[i]))
+    
+        
+    # TODO
+    # Get only the link cords where the weights are more than 0.09
+    node_points = go.Scatter3d(
+        x=x,
+        y=y,
+        z=z,
+        mode='markers',
+        name='actors',
+        marker = go.Marker(
+            symbol='circle',
+            size=13,
+            colorscale="viridis",
+            opacity = 0.8),
+        text=neuron_ids,
+        hoverinfo='text'  
+    )
+    
+    fig = go.Figure(data=node_points)
+    for start in link_coords:
+        for end in link_coords:
+            fig.add_trace(
+                go.Scatter3d(
+                    x=[start[0], end[0]], 
+                    y=[start[1], end[1]], 
+                    z=[start[2], end[2]],
+                    mode='lines',
+                    showlegend=False,
+                    hoverinfo='none',
+                    name=""
+                ),
+            )
+
+    fig.show()
 if __name__ == "__main__":
     #NeuronSpikes()
-    NeuronPotentials()
+    #NeuronPotentials()
+    Network()
