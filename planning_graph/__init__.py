0250736813c57a2be014d7f7457484e6e73c0503
Alan Chang <76526328+dreamsproxy@users.noreply.github.com>
2026-07-17 01:58:30 +0800

    feat: export planning group model

--- planning_graph/__init__.py
+++ planning_graph/__init__.py
@@ -1,23 +1,27 @@
 """LiSNN U1 planning graph editor."""
 
 from .models import (
+    DEFAULT_GROUP_COLOR,
     DOCUMENT_VERSION,
     EDGE_TYPES,
     NODE_KINDS,
     NODE_STATUSES,
     PlanningDocument,
     PlanningEdge,
+    PlanningGroup,
     PlanningNode,
 )
 from .serialization import load_document, save_document
 
 __all__ = [
+    "DEFAULT_GROUP_COLOR",
     "DOCUMENT_VERSION",
     "EDGE_TYPES",
     "NODE_KINDS",
     "NODE_STATUSES",
     "PlanningDocument",
     "PlanningEdge",
+    "PlanningGroup",
     "PlanningNode",
     "load_document",
     "save_document",
