bc9253f3c9b0102acf0813b1dbb874f285c360b0
Alan Chang <76526328+dreamsproxy@users.noreply.github.com>
2026-07-16 22:19:54 +0800

    feat: export LiSNN hypothesis substrate

--- substrates/__init__.py
+++ substrates/__init__.py
@@ -1,5 +1,6 @@
 """Cognitive substrate implementations."""
 
+from .lisnn_hypothesis import LiSNNHypothesisSubstrate
 from .rule_baseline import RuleHypothesisTracker
 
-__all__ = ["RuleHypothesisTracker"]
+__all__ = ["LiSNNHypothesisSubstrate", "RuleHypothesisTracker"]
